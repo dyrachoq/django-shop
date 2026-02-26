379385e0a34c4965593fb81c94f79270be96610c
dyrachoq <plexus4mal@gmail.com>
2026-02-26 14:49:16 +0300

    views и шаблоны

--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Category, Product
+
+
+
+
+def product_list(request, category_slug=None): #request - клиент, всегда пишется
+    categories = Category.objects.all() #берем все категории из бд
+    products = Product.objects.filter(available = True)
+    
+    category = None 
+    
+    if category_slug:
+        category = get_object_or_404(Category, category_slug=category_slug)
+        products = products.filter(category=category)
+        
+    return render(request, 'main/product/list.html', #в какой файл заносим данные
+                  {'category': category,  #что кладем в файл html
+                   'categories': categories,
+                   'products': products})
+
